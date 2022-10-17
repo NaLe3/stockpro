668e5b4e96b6646a0e8c0c408a718007d11e5425
Nathan Lebas <nathanlebas3@gmail.com>
2022-10-17 15:15:51 +0200

    Artisans CRUD

---
+++ app/controllers/artisans_controller.rb
@@ -0,0 +1,33 @@
+class ArtisansController < ApplicationController
+
+  def index 
+    @artisans = Artisan.all
+  end 
+
+  def show
+    @artisan = Artisan.find(params[:id])
+  end
+
+  def new
+    @artisan = Artisan.new
+  end
+
+  def create
+    @artisan = Artisan.create(artisan_params)
+    redirect_to artisans_path
+  end
+
+  def destroy
+    @artisan = Artisan.find(params[:id])
+    @artisan.destroy
+    redirect_to artisans_path
+  end
+
+  private
+
+  def artisan_params
+    params.require(:artisan).permit(:name, :first_name, :tjm, :desc)
+  end
+
+
+end
