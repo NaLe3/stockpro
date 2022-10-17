653ee97991f47bef2f8e3a7b9ed7e27bcc61df60
Nathan Lebas <nathanlebas3@gmail.com>
2022-10-17 17:50:20 +0200

    Devise implementation to view missions

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,5 +1,7 @@
 class MissionsController < ApplicationController
 
+  before_action :authenticate_user!, only: :index
+
   def index 
     @missions = Mission.where(artisan_id: params[:artisan_id])
     @artisan =  Artisan.find(params[:artisan_id])
